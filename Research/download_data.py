18bfec3c52edbd05ae68a73ad86ccf55dc0d969e
jimvan0211 <105726072+jimvan0211@users.noreply.github.com>
2023-12-26 20:34:19 +0800

    新增 资产负债表、利润表、现金流量表 数据
    新增: 季频盈利能力/季频营运能力/季频成长能力/季频偿债能力/季频现金流量
    详见:
    http://baostock.com/baostock/index.php/Python_API%E6%96%87%E6%A1%A3#.E6.9F.A5.E8.AF.A2.E5.AD.A3.E9.A2.91.E8.B4.A2.E5.8A.A1.E6.95.B0.E6.8D.AE.E4.BF.A1.E6.81.AF

--- Research/download_data.py
+++ Research/download_data.py
@@ -1,42 +1,89 @@
 import baostock as bs
 import pandas as pd
 
-START_DATE='2023-09-01'
-END_DATE='2023-09-28'
 
-code_df = pd.read_csv('./Data/stock_industry.csv', encoding='gbk')
-industry_df = code_df.loc[code_df['industry'] == '钢铁']    # 筛选出钢铁行业
-industry_code_df = industry_df['code']  # 获取钢铁行业各只股票的code
-code_list = industry_code_df.to_list()
+START_DATE = '2023-09-01'
+END_DATE = '2023-09-28'
+INDUSTRY = '钢铁'
+PATH_DATA = "./Data/"
+PATH_STOCK_INDUSTRY = PATH_DATA + "stock_industry.csv"
+PATH_K_DATA = PATH_DATA + "k_data.csv"
+PATH_PROFIT_DATA = PATH_DATA + "profit_data.csv"
+PATH_OPERATION_DATA = PATH_DATA + "operation_data.csv"
+PATH_GROWTH_DATA = PATH_DATA + "growth_data.csv"
+PATH_BALANCE_DATA = PATH_DATA + "balance_data.csv"
+PATH_CASH_FLOW_DATA = PATH_DATA + "cash_flow_data.csv"
 
-#### 登陆系统 ####
-lg = bs.login()
-# 显示登陆返回信息
-print('login respond error_code:'+lg.error_code)
-print('login respond  error_msg:'+lg.error_msg)
 
-#### 获取沪深A股历史K线数据 ####
-# 详细指标参数，参见“历史行情指标参数”章节；“分钟线”参数与“日线”参数不同。“分钟线”不包含指数。
-# 分钟线指标：date,time,code,open,high,low,close,volume,amount,adjustflag
-# 周月线指标：date,code,open,high,low,close,volume,amount,adjustflag,turn,pctChg
-data_list = []
-result = pd.DataFrame()
-for ele_code in code_list:
-    rs = bs.query_history_k_data_plus(ele_code,
-                                      "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,isST",
-                                      start_date=START_DATE, end_date=END_DATE,
-                                      frequency="d", adjustflag="3")
-    print('query_history_k_data_plus respond error_code:'+rs.error_code)
-    print('query_history_k_data_plus respond  error_msg:'+rs.error_msg)
+def login():
+    # 登陆系统
+    lg = bs.login()
 
-    #### 打印结果集 ####
-    while (rs.error_code == '0') & rs.next():
-        # 获取一条记录，将记录合并在一起
-        data_list.append(rs.get_row_data())
-result = pd.DataFrame(data_list, columns=rs.fields)     # type: ignore
+    # 显示登陆返回信息
+    print('login respond error_code:' + lg.error_code)
+    print('login respond  error_msg:' + lg.error_msg)
+    
 
-#### 结果集输出到csv文件 ####
-result.to_csv("./Data/history_A_stock_k_data.csv", index=False)
+# 读取目标行业的股票代码列表
+def search_code():
+    # 读取股票行业数据，这里假设已有一个包含股票代码和行业信息的CSV文件
+    code_df = pd.read_csv(PATH_STOCK_INDUSTRY, encoding='gbk')
 
-#### 登出系统 ####
-bs.logout()
+    # 筛选出特定行业
+    industry_df = code_df.loc[code_df['industry'] == INDUSTRY]
+
+    # 获取行业内所有股票的代码列表
+    industry_code_list = industry_df['code'].tolist()
+    return industry_code_list
+
+
+# 通过api查询
+def query_api_data(codes, api_func, args, path):
+    datas = []
+    result = pd.DataFrame()
+    
+    # 下载5年内季度财报
+    if not api_func == bs.query_history_k_data_plus:
+        for code in codes:
+            for year in range(2018, 2024):
+                for quarter in range(1, 5):
+                    result = api_func(code=code, year=year, quarter=quarter)
+                    print(f'{api_func} respond error_code:'+result.error_code)
+                    print(f'{api_func} respond  error_msg:'+result.error_msg)
+                    while (result.error_code == '0') & result.next():
+                        datas.append(result.get_row_data())
+        result_df = pd.DataFrame(datas, columns=result.fields)
+        result_df.to_csv(path, index=False)
+        return
+    
+    # 下载日k线数据
+    for code in codes:
+        result = api_func(code=code, **args)
+        print(f'{api_func} respond error_code:'+result.error_code)
+        print(f'{api_func} respond  error_msg:'+result.error_msg)
+        while (result.error_code == '0') & result.next():
+            datas.append(result.get_row_data())
+
+    result_df = pd.DataFrame(datas, columns=result.fields)
+    result_df.to_csv(path, index=False)
+
+
+if __name__ == '__main__':
+    login()
+    codes = search_code()
+
+    # 查询历史A股K线数据
+    query_api_data(codes, api_func=bs.query_history_k_data_plus, args={"fields": "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,isST", "start_date": START_DATE, "end_date": END_DATE, "frequency": "d", "adjustflag": "3"},  path=PATH_K_DATA)
+    # 查询盈利能力数据
+    query_api_data(codes, api_func=bs.query_profit_data, args={}, path=PATH_PROFIT_DATA)
+    # 查询营运能力数据...
+    query_api_data(codes, api_func=bs.query_operation_data, args={}, path=PATH_OPERATION_DATA)
+    # 查询成长能力数据
+    query_api_data(codes, api_func=bs.query_growth_data, args={}, path=PATH_GROWTH_DATA)
+    # 查询偿债能力数据
+    query_api_data(codes, api_func=bs.query_balance_data, args={}, path=PATH_BALANCE_DATA)
+    # 查询现金流量数据
+    query_api_data(codes, api_func=bs.query_cash_flow_data, args={}, path=PATH_CASH_FLOW_DATA)
+
+    # 登出系统
+    bs.logout()
